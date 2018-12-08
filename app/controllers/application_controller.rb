6f4e80232ff6ab77cc2fad8ec9736be4783d2cc7
tom.ataru <ataru.tomita@gmail.com>
2018-12-08 09:11:07 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,10 @@ class ApplicationController < ActionController::Base
     end
   end
   
+
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
-
 end
